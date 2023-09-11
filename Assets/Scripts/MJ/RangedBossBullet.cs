852cbaa026091ea41d09808697e710f12b7ac492
Mintti <mintti97@gmail.com>
2023-09-11 16:22:58 +0900

    [Fix] 탄환 사라지지 않는 오류 수정

--- Assets/Scripts/MJ/RangedBossBullet.cs
+++ Assets/Scripts/MJ/RangedBossBullet.cs
@@ -8,12 +8,14 @@ public class RangedBossBullet : MonoBehaviour
     [SerializeField] private int m_damage;
     CircleCollider2D m_collider2D;
     Animator m_animator;
-    
-    
+    private static readonly int Destory = Animator.StringToHash("Destory");
+
+
     // Start is called before the first frame update
     void Start()
     {
         m_collider2D = GetComponent<CircleCollider2D>();
+        m_animator = GetComponent<Animator>();
     }
 
     public void Init(float _bulletSpeed)
@@ -36,12 +38,15 @@ public class RangedBossBullet : MonoBehaviour
 
     void PlayDestroyAnimation()
     {
+        
+        GetComponent<Rigidbody2D>().velocity = transform.right * 0 ;
+        m_animator.SetBool(Destory, true);
         m_collider2D.enabled = false;
     }
 
     public void DestroyObjectAfterAnimation()
     {
-        Destroy(this);
+        Destroy(this.gameObject);
     }
     
 }
