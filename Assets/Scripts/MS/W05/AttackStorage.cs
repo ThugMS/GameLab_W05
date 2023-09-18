8ebf3324fb77f68bf2f9becf470ace439ca49382
MinseokLee <leems041625@gmail.com>
2023-09-18 13:05:09 +0900

    [Feat] : 일렉트릭 효과 콜라이더 구현 완료

--- Assets/Scripts/MS/W05/AttackStorage.cs
+++ Assets/Scripts/MS/W05/AttackStorage.cs
@@ -9,26 +9,51 @@ public class AttackStorage : MonoBehaviour
     #endregion
 
     #region PrivateVariables
+    [SerializeField] LineRenderer m_line;
+    [SerializeField] Vector3[] m_points;
+    [SerializeField] List<Vector2> m_points2D = new List<Vector2>();
+    [SerializeField] Vector2[] m_pointsArr;
+    [SerializeField] EdgeCollider2D m_edgeCollider;
 
+    private int m_layerMask;
     #endregion
 
     #region PublicMethod
+    private void Start()
+    {
+        m_layerMask = LayerMask.GetMask("Monster", "Boss");
+    }
     private void Update()
     {
         if(m_isElectric == true)
         {
             Tear[] objs = gameObject.GetComponentsInChildren<Tear>();
+            m_points = new Vector3[objs.Length];
+            m_pointsArr = new Vector2[objs.Length]; 
 
-            for(int i=0;i<objs.Length-1; i++)
+            for (int i=0;i<objs.Length; i++)
             {
-                objs[i].SetElectric(objs[i+1].gameObject);
+                m_points[i] = objs[i].transform.position;
+                m_pointsArr[i] = objs[i].transform.position;
+
+                m_points2D.Add(objs[i].transform.position);
             }
+            m_line.positionCount = m_points.Length;
+            m_line.SetPositions(m_points);
+
+            m_edgeCollider.points = m_pointsArr;
         }
         
     }
     #endregion
 
     #region PrivateMethod
-
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if ((m_layerMask & (1 << collision.gameObject.layer)) != 0)
+        {
+            collision.gameObject.GetComponent<DamageBot>().ShowDamage(1);           
+        }
+    }
     #endregion
 }
